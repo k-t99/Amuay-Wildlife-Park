04742233b9a1e99cd949ab231e6124fd65ab87e5
k-t99 <k.lach1@universityofgalway.ie>
2025-11-18 21:35:56 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const filterButton = document.querySelectorAll(".filter-btn");
+  const animalCard = document.querySelectorAll(".animal-card");
+  });
+  if (!filterButtons.length || !animalCards.length) {
+  }
+
+filterButton.forEach(function (button) {
+  button.addEventListener("click", function () {
+    const type = button.getAttribute("data-type");
+    
+    filterButton.forEach(function (btn) {
+      btn.classList.remove("active");
+  });
+
+    button.classList.add("active");
+    
+    animalCard.forEach(function (card) {
+      const cardType = card.getAttribute("data-type);
+
+    if (type === "all" || type === cardType) {
+        card.style.distplay = "";
+        } else {
+          card.style.distplay = "non";
+        }
+      });
+    });
+  });
+});
+
+
+
 
