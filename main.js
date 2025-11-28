3b70089da33e7bbdf7122178e6cb5826ab1702fd
k-t99 <k.lach1@universityofgalway.ie>
2025-11-28 11:29:00 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+// The code will allow for the animal button to be active on the wildlife page.//
+
+document.addEventListener("load", function () {
+  const Quickfilter = document.querySelectorAll(".filter-btn");
+  const animalList = document.querySelectorAll(".animal-card");
+
+  if (!Quickfilter.length || !animalList.length) {
+  }
+
+Quickfilter.forEach(function (button) {
+  button.addEventListener("click", function () {
+    const type = button.getAttribute("data-type");
+    
+   Quickfilter.forEach(function (btn) {
+      btn.classList.remove("active");
+  });
+
+    button.classList.add("active");
+    
+    animalList.forEach(function (card) {
+      const cardType = card.getAttribute("data-type);
+
+    if (type === "all" || type === cardType) {
+        card.style.display = "";
+        } else {
+          card.style.distplay = "none";
+           });
+    });
+  });
+});
+
+
+
+
